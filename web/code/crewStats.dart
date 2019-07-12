a121de4fd9da58d8551117c4ad7a2f8a6691aa77
aCactusOrSomething <whudsonmiller@gmail.com>
2019-07-12 16:17:48 -0500

    beta stat 2 logic

--- web/code/crewStats.dart
+++ web/code/crewStats.dart
@@ -7,4 +7,34 @@
   | Blood  | Loyal      | Diplomacy   |FreeSpirited| Breath |
   | Space  | Patient    | Combat      | Impatient  | Time   |
   | Rage   | Realistic  | Sanity      | Idealistic | Hope   |
-  */
+
+  so how i see it, a "stat" class can be a value between -113 and 113.
+  i'll say that a check can be between anywere between 1 and 150, though usually they wouldn't go that high or that low
+
+*/
+
+import 'dart:math';
+
+class crewStat {
+  int value;
+  String name;
+
+  bool checkRoll(int check) {
+    //dont use seeded random probably unless its decided later to rework where the random comes from
+    Random rand = new Random();
+    if(value >= 0) {
+      int rolledValue = value + rand.nextInt(value ~/ 11.3);
+      if (rolledValue >= check)
+        return true;
+      else
+        return false;
+    } else {
+      int rolledValue = (value ~/ 11.3) + rand.nextInt(value);
+      if (rolledValue >= check)
+        return true;
+      else
+        return false;
+    }
+
+  }
+}
