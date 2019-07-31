645fd35e7d2760dd1bf04bd2d4d618b59ecf47d2
aCactusOrSomething <whudsonmiller@gmail.com>
2019-07-31 14:13:38 -0500

    notes for planet gen

---
+++ web/code/Planets.dart
@@ -0,0 +1,21 @@
+/*
+planets are gonna be pretty simple.
+  -distance from sun
+  -size
+  -composition
+    -rockiness
+    -water
+    -atmosphere
+
+  *conditions for life:
+    -water/atmosphere ratios are close to 1:1, with neither being at 0
+    -middle distance from sun
+    -average size
+
+  *add descriptions for what lifeforms are like probably
+
+ */
+
+class Planet {
+
+}
