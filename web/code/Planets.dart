2b7c17b536fdb8b36d5431885e5b45e08fedfb32
aCactusOrSomething <whudsonmiller@gmail.com>
2019-08-11 20:34:38 -0500

    planets are really just funky spaceships

--- web/code/Planets.dart
+++ web/code/Planets.dart
@@ -1,5 +1,8 @@
 import 'dart:core';
+import 'dart:async';
+import 'dart:math';
 import 'Room.dart';
+import 'Starship.dart';
 /*
 planets are gonna be pretty simple.
   -distance from sun
@@ -18,7 +21,39 @@ planets are gonna be pretty simple.
   *add descriptions for what lifeforms are like probably
 
  */
+//todo make Starship and Planet extend from the same generic class
+class Planet extends Starship{
+  int sunDistance; //sun distance is determined by solar system generator, since you can't have 2 planets in the same spot.
 
-class Planet {
-  int sunDistance;
+  //todo remember to allow for hardcoded planets, not just random ones!
+  Planet(int sunDistance, int seed):super(seed) {
+    this.sunDistance = sunDistance;
+  }
+
+  static Future<Planet> getRandomPlanet(int seed) async {
+    Planet planet = new Planet(0, seed);
+
+    Random rand = new Random(seed);
+
+    //determine size
+    bool isValid = false;
+    int size;
+    while (!isValid) {
+      size = rand.nextInt(10) + 2;
+      if (size >= 2) {
+        isValid = true;
+        planet.rooms = new List<PlanetTrait>(size);
+      }
+    }
+
+    //assign rooms
+    for (int i = 0; i < planet.rooms.length; i++) {
+      planet.rooms[i] = new PlanetTrait(rand.nextInt(PlanetTrait.PLANET_TRAITS.length));
+    }
+    planet.sortRooms();
+
+    await planet.setDescriptionAndName(rand);
+
+    return planet;
+  }
 }
